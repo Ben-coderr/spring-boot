88e5baeabe90a7aca947fb4627cc77dd1348f92e
Benkorich Abdenour <abdenour.benkorich@etu.univ-mosta.dz>
2025-04-18 18:02:33 +0100

    testing

--- src/main/java/com/school/system/controllers/hello.java
+++ src/main/java/com/school/system/controllers/hello.java
@@ -1,4 +1,13 @@
 package com.school.system.controllers;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class hello {
+    @GetMapping(path = "/hello")
+    public String helloWorld(){
+        return "wee chagotli";
+    }
 }
